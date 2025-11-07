daa7e3343fd2a4978f2b88b138be92c10d61b812
xtatrax <xtatrax@me.com>
2025-11-07 13:02:49 +0900

    python スクリプト追加

---
+++ run.py
@@ -0,0 +1,23 @@
+import http.server
+import socketserver
+import sys
+import os
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+if "darwin" in sys.platform :
+    print("OS is macOS")
+    os.system("open http://localhost:%d" % PORT)
+elif "win" in sys.platform :
+    print("OS is win")
+    os.system("start http://localhost:%d" % PORT)
+else:
+    print("未実装")
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
+
+
